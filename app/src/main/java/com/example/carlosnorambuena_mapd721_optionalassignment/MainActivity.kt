a3ef9fe09104af3b059b24d628b14b233419a967
Carlos Norambuena <cnoramb1@my.centennialcollege.ca>
2023-04-07 17:23:07 -0400

    SplashActivity

--- app/src/main/java/com/example/carlosnorambuena_mapd721_optionalassignment/MainActivity.kt
+++ app/src/main/java/com/example/carlosnorambuena_mapd721_optionalassignment/MainActivity.kt
@@ -11,31 +11,19 @@ import com.example.carlosnorambuena_mapd721_optionalassignment.Model.Places
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
-        const val INTENT_PARCELABLE = "OBJECT_INTENT"
-    }
+    companion object{const val INTENT_PARCELABLE = "OBJECT_INTENT"}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val imageList = listOf<Places>(
-            Places(
-                "Casa Loma",
-                R.drawable .casa_loma),
-            Places(
-                "Casa 1",
-                R.drawable .casa_loma),
-            Places(
-                "Casa 2",
-                R.drawable .casa_loma),
-            Places(
-                "Casa 3",
-                R.drawable .casa_loma),
-            Places(
-                "Casa 4",
-                R.drawable .casa_loma),
-            )
+            Places("Casa Loma", R.drawable .casa_loma),
+            Places("Casa 1", R.drawable .casa_loma),
+            Places("Casa 2", R.drawable .casa_loma),
+            Places("Casa 3", R.drawable .casa_loma),
+            Places("Casa 4", R.drawable .casa_loma),
+        )
 
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_View_Item)
